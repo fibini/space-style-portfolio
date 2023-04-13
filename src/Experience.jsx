448cc91098c314a65561cd83f5d2ebc57a003eb0
fibini <fabienswift@gmail.com>
2023-04-13 15:26:41 -0400

    add text to experience , so user can rotate when using mobile device

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,7 @@
 import { Perf } from "r3f-perf";
 import Homepage from "./Homepage";
 import Portal from "./Portal";
+import { Text } from "@react-three/drei";
 
 
 export default function Experience()
@@ -11,5 +12,11 @@ export default function Experience()
             <Perf />
             <Portal/>
             <Homepage />
+            <Text
+                fontSize={0.5}
+                position={[0, -5.5, 0]}
+            >
+                Please rotate mobile device to enjoy full screen experience
+            </Text>
     </>
 }
