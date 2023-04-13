da8ac97a82a6f63e84d6a2ca414a4510841ec271
fibini <fabienswift@gmail.com>
2023-04-13 13:24:28 -0400

    delete useless imports

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,21 +1,15 @@
 import { Perf } from "r3f-perf";
-import About from "./About";
 import Homepage from "./Homepage";
-import Projects from "./Projects";
-import Contacts from "./Contact";
 import Portal from "./Portal";
-import Astro from "./Astro";
+
 
 export default function Experience()
 {
 
     return <>
-    {/* <OrbitControls /> */}
-    <ambientLight />
-    <Perf />
+            <ambientLight />
+            <Perf />
             <Portal/>
             <Homepage />
-            {/* <Projects /> */}
-            {/* <Contacts /> */}
     </>
 }
